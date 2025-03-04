bd498c4bfd6a2d0d56add9dfb71e1e04637d94ba
mfoltz <mitchell.foltz@gmail.com>
2025-03-03 22:54:14 -0600

    - minor changes, next need to see about direction facing unless they just face last direction interacted with

--- Plugin.cs
+++ Plugin.cs
@@ -92,10 +92,202 @@ internal class Plugin : BasePlugin
 
         Core.Log.LogInfo($"{MyPluginInfo.PLUGIN_NAME}[{MyPluginInfo.PLUGIN_VERSION}] loaded!");
     }
+
     static void InitConfig()
     {
         // CreateDirectory(_configFile);
 
+        _merchantRestockTimes = InitConfigEntry(
+            "General",
+            "RestockTime",
+            "15,15,15,15,15",
+            "The restock time in minutes for merchants. 0 is no restocking maybe? Idk, guess and check, this isn't released to the public for a reason :P"
+        );
+
+        // ------------------- First Merchant -------------------
+        _firstMerchantOutputItems = InitConfigEntry(
+            "FirstMerchant",
+            "OutputItems",
+            "1247086852,2019195024,-1619308732,-222860772,-651878258,-651878258,-651878258,-651878258,-1583485601,-223452038,-77477508,1270271716",
+            "The item prefabs for the first merchant's output."
+        );
+
+        _firstMerchantOutputAmounts = InitConfigEntry(
+            "FirstMerchant",
+            "OutputAmounts",
+            "1,1,1,1,3,3,3,3,900,10,1,100",
+            "The item amounts for the first merchant's output."
+        );
+
+        _firstMerchantInputItems = InitConfigEntry(
+            "FirstMerchant",
+            "InputItems",
+            "666638454,-21943750,-1581189572,-1260254082,-1260254082,-1581189572,-21943750,666638454,-257494203,660533034,-223452038,-257494203",
+            "The item prefabs for the first merchant's input."
+        );
+
+        _firstMerchantInputAmounts = InitConfigEntry(
+            "FirstMerchant",
+            "InputAmounts",
+            "7,7,7,7,5,5,5,5,300,25,300,900",
+            "The item amounts for the first merchant's input."
+        );
+
+        _firstMerchantStockAmounts = InitConfigEntry(
+            "FirstMerchant",
+            "StockAmounts",
+            "99,99,99,99,99,99,99,99,99,99,99,99",
+            "The stock amounts for the first merchant's outputs."
+        );
+
+        // ------------------- Second Merchant -------------------
+        _secondMerchantOutputItems = InitConfigEntry(
+            "SecondMerchant",
+            "OutputItems",
+            "-570287766,447901086,-149778795,736318803,-1779269313,177845365,67930804,-1642545082,1332980397,-1805325497,1686577386,1501868129",
+            "The item prefabs for the second merchant's output."
+        );
+
+        _secondMerchantOutputAmounts = InitConfigEntry(
+            "SecondMerchant",
+            "OutputAmounts",
+            "10,10,10,10,10,10,10,10,900,900,350,900",
+            "The item amounts for the second merchant's output."
+        );
+
+        _secondMerchantInputItems = InitConfigEntry(
+            "SecondMerchant",
+            "InputItems",
+            "28625845,28625845,28625845,-949672483,-949672483,-571562864,-571562864,-571562864,-257494203,-257494203,-257494203,-257494203",
+            "The item prefabs for the second merchant's input."
+        );
+
+        _secondMerchantInputAmounts = InitConfigEntry(
+            "SecondMerchant",
+            "InputAmounts",
+            "300,300,300,200,200,410,410,210,250,150,480,200",
+            "The item amounts for the second merchant's input."
+        );
+
+        _secondMerchantStockAmounts = InitConfigEntry(
+            "SecondMerchant",
+            "StockAmounts",
+            "99,99,99,99,99,99,99,99,99,99,99,99",
+            "The stock amounts for the second merchant's outputs."
+        );
+
+        // ------------------- Third Merchant -------------------
+        _thirdMerchantOutputItems = InitConfigEntry(
+            "ThirdMerchant",
+            "OutputItems",
+            "442700150,2061238391,-1638796801,285875674,-1810734832,1040125618,886814985,1271087499,1717016192,1048769481,1490846791,-1527315816",
+            "The item prefabs for the third merchant's output."
+        );
+
+        _thirdMerchantOutputAmounts = InitConfigEntry(
+            "ThirdMerchant",
+            "OutputAmounts",
+            "1,1,1,1,1,1,1,1,1,1,1,650",
+            "The item amounts for the third merchant's output."
+        );
+
+        _thirdMerchantInputItems = InitConfigEntry(
+            "ThirdMerchant",
+            "InputItems",
+            "-2147445292,572026243,747911021,649637190,1963988265,-1038642372,-413259500,3759455,2142983740,-1421775051,-1222824286,-257494203",
+            "The item prefabs for the third merchant's input."
+        );
+
+        _thirdMerchantInputAmounts = InitConfigEntry(
+            "ThirdMerchant",
+            "InputAmounts",
+            "5,5,5,5,5,5,5,5,5,5,5,550",
+            "The item amounts for the third merchant's input."
+        );
+
+        _thirdMerchantStockAmounts = InitConfigEntry(
+            "ThirdMerchant",
+            "StockAmounts",
+            "99,99,99,99,99,99,99,99,99,99,99,99",
+            "The stock amounts for the third merchant's outputs."
+        );
+
+        // ------------------- Fourth Merchant -------------------
+        _fourthMerchantOutputItems = InitConfigEntry(
+            "FourthMerchant",
+            "OutputItems",
+            "442700150,2061238391,-1638796801,285875674,-1810734832,1040125618,886814985,1271087499,1717016192,1048769481,1490846791,-1748835106",
+            "The item prefabs for the fourth merchant's output."
+        );
+
+        _fourthMerchantOutputAmounts = InitConfigEntry(
+            "FourthMerchant",
+            "OutputAmounts",
+            "1,1,1,1,1,1,1,1,1,1,1,650",
+            "The item amounts for the fourth merchant's output."
+        );
+
+        _fourthMerchantInputItems = InitConfigEntry(
+            "FourthMerchant",
+            "InputItems",
+            "442700150,2061238391,-1638796801,285875674,-1810734832,1040125618,886814985,1271087499,1717016192,1048769481,1490846791,-257494203",
+            "The item prefabs for the fourth merchant's input."
+        );
+
+        _fourthMerchantInputAmounts = InitConfigEntry(
+            "FourthMerchant",
+            "InputAmounts",
+            "2,2,2,2,2,2,2,2,2,2,2,1440",
+            "The item amounts for the fourth merchant's input."
+        );
+
+        _fourthMerchantStockAmounts = InitConfigEntry(
+            "FourthMerchant",
+            "StockAmounts",
+            "99,99,99,99,99,99,99,99,99,99,99,99",
+            "The stock amounts for the fourth merchant's outputs."
+        );
+
+        // ------------------- Fifth Merchant -------------------
+        _fifthMerchantOutputItems = InitConfigEntry(
+            "FifthMerchant",
+            "OutputItems",
+            "2099198078,781586362,1272855317,1102277512,-915028618,1630030026,1801132968,-1930402723,1954207008,124616797,220001518,950358400",
+            "The item prefabs for the fifth merchant's output."
+        );
+
+        _fifthMerchantOutputAmounts = InitConfigEntry(
+            "FifthMerchant",
+            "OutputAmounts",
+            "1,1,1,1,1,1,1,1,1,1,1,1",
+            "The item amounts for the fifth merchant's output."
+        );
+
+        _fifthMerchantInputItems = InitConfigEntry(
+            "FifthMerchant",
+            "InputItems",
+            "442700150,2061238391,-1638796801,285875674,-1810734832,1040125618,886814985,1271087499,1271087499,1717016192,1048769481,1490846791",
+            "The item prefabs for the fifth merchant's input."
+        );
+
+        _fifthMerchantInputAmounts = InitConfigEntry(
+            "FifthMerchant",
+            "InputAmounts",
+            "5,5,5,5,5,5,5,5,5,5,5,5",
+            "The item amounts for the fifth merchant's input."
+        );
+
+        _fifthMerchantStockAmounts = InitConfigEntry(
+            "FifthMerchant",
+            "StockAmounts",
+            "99,99,99,99,99,99,99,99,99,99,99,99",
+            "The stock amounts for the fifth merchant's outputs."
+        );
+    }
+
+    /*
+    static void InitConfig()
+    {
         _merchantRestockTimes = InitConfigEntry("General", "RestockTime", "0,0,0,0,0", "The restock time in minutes for merchants. 0 is no restocking maybe? Idk, guess and check, this isn't released to the public for a reason :P");
         
         _firstMerchantOutputItems = InitConfigEntry("FirstMerchant", "OutputItems", "1247086852,-1619308732,2019195024,-222860772,950358400,220001518,124616797,1954207008,-1930402723,1801132968,1630030026,-915028618,1102277512,1272855317,781586362,2099198078", "The item prefabs for the first merchant's output.");
@@ -128,6 +320,7 @@ internal class Plugin : BasePlugin
         _fifthMerchantInputAmounts = InitConfigEntry("FifthMerchant", "InputAmounts", "250,250,250,250,250", "The item amounts for the fifth merchant's input.");
         _fifthMerchantStockAmounts = InitConfigEntry("FifthMerchant", "StockAmounts", "99,99,99,99,99", "The stock amounts for the fifth merchant's outputs.");
     }
+    */
     static ConfigEntry<T> InitConfigEntry<T>(string section, string key, T defaultValue, string description)
     {
         // Bind the configuration entry and get its value
